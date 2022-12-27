6f6d6451924a8988883e2f13d70550b2a4b66591
crackue <crackue@gmail.com>
2022-12-27 19:16:21 +0200

    fix

--- bot_app/handlers/handlers/ask_handler.py
+++ bot_app/handlers/handlers/ask_handler.py
@@ -1,12 +1,17 @@
 import openai
+import logging
 from telegram import Update
 from telegram.ext import (CallbackContext, ConversationHandler)
+
+
+logger = logging.getLogger(__name__)
 
 
 def ask(update: Update, context: CallbackContext):
     username = update.message.from_user['username']
     prompt = update.message['text']
     response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0, max_tokens=7)
-    update.message.reply_text(f"response for {username}: {response.choices[0].text}")
+    for choice in response.choices:
+        logger.info(f"response {response.choices[0].text}")
+        update.message.reply_text(f"response for {username}:\nchoice {choice.index}: {choice.text}")
     return ConversationHandler.END
-
